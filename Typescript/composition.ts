38299b8ef6289c3249fe550060c65fdc2a78fe75
Stefan Heyder <stefan.heyder@gmail.com>
2017-10-11 11:07:45 +0200

    Move composition to its directory

--- Typescript/composition.ts
+++ Typescript/composition.ts
@@ -1,5 +1,7 @@
 export const compose = (f,g) => x => g(f(x));
 
+export const composeMany = (f, ...gs) => gs.length == 0 ? f : compose(composeMany(gs),f);
+
 const f = x => x * x
 const g = x => x + 1
 
